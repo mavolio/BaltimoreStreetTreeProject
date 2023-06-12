453bc2ced24fc4ef9ccfbd0e3d63e0ccd2a1795e
Trini Fleming <trinifleming@gmail.com>
2023-06-12 16:57:58 -0400

    troubleshooting neighborhood data

--- Baltimore neighborhoods.R
+++ Baltimore neighborhoods.R
@@ -2,13 +2,19 @@
 #started 6/12
 library(tidyverse)
 library(sf)
+library(mapview)
 setwd("C:\\Users\\trini\\Documents\\Street Trees")
 
 #reading in shapefiles
-bmore_nb <- st_read("Neighborhood.shp")
 street_tree <- st_read("bc_forestry_trees_20190319.shp")
+# bmore_nb <- st_read("Neighborhood.shp") |> st_transform(crs = st_crs(street_tree))
+bmore_nb <- st_read("Neighborhood.geojson") |> st_transform(crs = st_crs(street_tree))
+
 
 #combining files
 combined <- street_tree %>%
   st_join(bmore_nb) %>%
   st_drop_geometry()
+
+street_tree %>% sample_n(100) %>% mapview()
+bmore_nb %>% mapview()
