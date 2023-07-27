e705cb3a35a134fb726dc85e70da867eee4443ee
Trini Fleming <trinifleming@gmail.com>
2023-07-27 09:40:31 -0400

    Working on loop system in the temperature reconciler.

--- scripts/Temperature.R
+++ scripts/Temperature.R
@@ -1,35 +1,47 @@
 library(sf); library(tidyverse)
 
-heat_severity <- 
-  raster::raster('../../BaltimoreTrees/BaltimoreStreetTreeProject_Large_Data/Urban Heat Island Mapping/Urban Heat Island Severity for U.S. cities - 2019.tiff') #temporarily using this data because it included a raster file
 
 
+#PM Baltimore temperature raster
+temperature <- 
+  raster::raster('../../BaltimoreTrees/BaltimoreStreetTreeProject_Large_Data\\baltimore\\temperature surfaces\\bal_pm.tif')
+ 
 
+#BNIA Neighborhoods 
 bmore_nb <- 
   st_read("../../BaltimoreTrees/BaltimoreStreetTreeProject_Large_Data/Community Statistical Areas (BNIA neighborhoods)/Community_Statistical_Areas_(CSAs)__Reference_Boundaries.shp") |> 
   st_transform(crs = st_crs(temperature))
 
+#loop
 tictoc::tic(); for(i in bmore_nb$Community){
   print(i)
   
-  vals <- 
-    terra::mask(terra::crop(heat_severity, bmore_nb |> filter(Community == i)), bmore_nb |> filter(Community == i)) |> 
-    raster::getValues()
-  
-  vals |>
-    summary() |>
-    as_tibble() |> 
-    mutate(  id = i
-             , severity_class = #Fix- would I have to use this for this data? Severity ranges 1-5
-               case_when( 
-                 vals == '1' ~                            
-                 , vals == '2' ~                
-                 , vals == '3' ~                      
-                 , vals == '4' ~                    
-                 , vals == '5' ~                      
-                 , TRUE ~ NA
-               )
-    ) |> 
-    select(id, n, severity_class) 
+   values <- 
+     terra::mask(terra::crop(temperature, bmore_nb |> filter(Community == i)), bmore_nb |> filter(Community == i)) |> 
+     raster::getValues()
+   
+#TO FIX lines 23-32 - I used a slightly different approach than what we discussed I think, is there a better way to do this?
+   values |>
+     table() |> 
+     as_tibble() |>
+     mutate(Neighborhood = i, 
+            avg_temp = mean(values, na.rm = TRUE)) |>
+     select(Neighborhood, n, avg_temp) #|>
+    #readr::write_csv(paste0('../../BaltimoreTrees/BaltimoreStreetTreeProject_Large_Data/baltimore/temperature surfaces/NB_pm_temp_test', Sys.Date(), '.csv') # I have this commented out until I'm sure about the loop system
+#                    , append = TRUE)
   
 }; tictoc::tic()
+
+
+#pivot still a work in progress 
+(test <- 
+    read_csv('../../BaltimoreTrees/BaltimoreStreetTreeProject_Large_Data/baltimore/temperature surfaces/NB_pm_temp_test2023-07-26.csv' 
+             , col_names = c('id', 'n', 'avg_temp')) |> 
+#    tidylog::filter(max_temp != 0) |> 
+    tidylog::filter(!is.na(max_temp)) |> 
+    tidylog::pivot_wider(names_from = , values_from = n, values_fill = 0)) #%>% # need old pipe
+#    tidylog::mutate(total_area_in_pixels = rowSums(.[,-1]))                           # for dot notation to work here
+
+
+test |> glimpse()
+
